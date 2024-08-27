cbdbcdc7fb5b7d7ac31997e1a02ddd3cfbce524c
JungJik Lee <fnwinter@gmail.com>
2024-08-27 23:48:48 +0900

    login

--- cnas/cnas.py
+++ cnas/cnas.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World! </p>"
+    return render_template('login.html')
 
