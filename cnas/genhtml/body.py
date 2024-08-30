77f356de30e22954a7e6429713c4b360e6dba23f
JungJik Lee <fnwinter@gmail.com>
2024-08-31 02:48:04 +0900

    test

--- cnas/genhtml/body.py
+++ cnas/genhtml/body.py
@@ -2,6 +2,16 @@ from genhtml.tag import tag
 
 class body(tag):
   def __init__(self):
+    """
+    create body tag
+
+    >>> from genhtml.body import body
+    >>> _body = body()
+    >>> _body.output()
+    '<body></body>'
+
+    """
+
     super().__init__()
     self.tags = ["<body>", "</body>"]
 
