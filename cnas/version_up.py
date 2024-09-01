6777f5585adefeb32bf04449812c70358777b43d
JungJik Lee <fnwinter@gmail.com>
2024-09-01 09:00:06 +0900

    update version 0.0.9

--- cnas/version_up.py
+++ cnas/version_up.py
@@ -1,17 +1,31 @@
 #!/bin/python3
 import os
+import subprocess
 
-VERSION = "0.0.7"
+VERSION = "0.0.10"
 
 def increase_version():
     version_parts = VERSION.split(".")
     version_parts[-1] = str(int(version_parts[-1]) + 1)
     return ".".join(version_parts)
 
+
+def make_commit():
+    execute_command(f"git add --all")
+    execute_command(f"git commit -m \"update version {VERSION}\"")
+    execute_command(f"git push origin main")
+
+def execute_command(command):
+    try:
+        subprocess.check_output(command, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"error while fixing pylint {str(e)}")
+
 if __name__ == "__main__":
     script_absolute_path = os.path.abspath(__file__)
     with open(script_absolute_path, "r+", encoding="utf-8") as f:
         lines = f.readlines()
         f.seek(0)
-        lines[3] = f"VERSION = \"{increase_version()}\"\n"
+        lines[4] = f"VERSION = \"{increase_version()}\"\n"
         f.write("".join(lines))
+    make_commit()
