bd7ba84ddf76650515f5e04b9cd0497786bf62a2
JungJik Lee <fnwinter@gmail.com>
2024-09-04 20:16:22 +0900

    fix nits

--- cnas/pages/index.py
+++ cnas/pages/index.py
@@ -3,7 +3,7 @@ from pages.page import page
 from genhtml.html import html
 from genhtml.head import head
 from genhtml.body import body
-from genhtml.paragraph import p
+from cnas.genhtml.para import para
 
 
 class index(page):
@@ -13,6 +13,6 @@ class index(page):
     def __str__(self):
         with html(head(cnas_title="cnas")) as _html:
             with body() as _body:
-                _body.append(p(cnas_text="index"))
+                _body.append(para(content="index"))
             _html.append(_body)
             return str(_html)
