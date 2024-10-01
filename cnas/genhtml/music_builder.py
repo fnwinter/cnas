2a06f86f8a72086bf134b844037622ded70e1090
JungJik Lee <fnwinter@gmail.com>
2024-10-02 00:55:08 +0900

    music

---
+++ cnas/genhtml/music_builder.py
@@ -0,0 +1,53 @@
+from genhtml.w3c.tag import tag
+from genhtml.w3c.figure import figure
+from genhtml.w3c.div import div
+from genhtml.w3c.image import image
+
+__MUSIC_CARD__ =\
+"""
+<div class="card" style="width:256px;margin:5px;">
+  <div class="card-image">
+    <figure class="imagei is-4by3">
+      <img
+        src="https://bulma.io/assets/images/placeholders/256x256.png"
+        alt="Placeholder image"
+      />
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img
+            src="https://bulma.io/assets/images/placeholders/96x96.png"
+            alt="Placeholder image"
+          />
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">John Smith</p>
+        <p class="subtitle is-6">@johnsmith</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+      iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+      <a href="#">#responsive</a>
+      <br />
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+    </div>
+  </div>
+</div>
+"""
+
+class music_builder(tag):
+    auto_index = 0
+
+    def __init__(self, *args, **kwargs):
+        # no inheritance
+        super().__init__([], {})
+        self.set_content(__MUSIC_CARD__)
+
+    def __str__(self):
+          return super().__str__() + "\n"
--- cnas/pages/music.py
+++ cnas/pages/music.py
@@ -1,41 +1,69 @@
+import os
+
 from pages.page import page
+from pages.error import error
+
+from util.file_util import is_image_file
+from util.config_path import get_gallery_path
+from util.system_path import get_gallery_thumbnail_path
+from util.path_util import rel_path
 
 from genhtml.w3c.html import html
-from genhtml.w3c.body import body
-from genhtml.w3c.para import para
+from genhtml.w3c.section import section
 from genhtml.w3c.div import div
-
-from genhtml.w3c.script import script
-from genhtml.w3c.button import button
+from genhtml.w3c.para import para
+from genhtml.w3c.br import br
 
 from genhtml.head_builder import head_builder
+from genhtml.body_builder import body_builder
+from genhtml.navibar_builder import navibar_builder
+from genhtml.music_builder import music_builder
+
+from genhtml.footer_builder import footer_builder
+
+from genhtml.bulma.image_modal import image_modal
+
 
 class music(page):
     def __init__(self):
         pass
 
     def __str__(self):
-        with html(head_builder()) as _html:
-            with body() as _body:
-                _body.append(para(content="index"))
-                _body.append(
-                    script(
-                        type_="py",
-                        src="static/main.py",
-                        config="static/pyscript.toml"
-                    )
-                )
-                _body.append(
-                    button(
-                        id_="get_joke",
-                        py_click="get_joke"
-                    ).set_content("button")
-                )
-                _body.append(
-                    div(
-                        id_="jokes"
-                    )
-                )
+        _title_div = div(
+            para(class_="title is-1 is-spaced").set_content("Music"),
+            para(class_="subtitle is-3").set_content(f"/music/path"),
+            br()
+        )
 
-            _html.append(_body)
-            return str(_html)
+        _div = div(class_="columns is-multiline")
+        _div.append(music_builder())
+        _div.append(music_builder())
+        _div.append(music_builder())
+        _div.append(music_builder())
+        _div.append(music_builder())
+        _div.append(music_builder())
+        _div.append(music_builder())
+
+        _section = section(
+            div(_title_div, class_="container").append(_div),
+            class_="section")
+
+        _modal = image_modal()
+
+        return str(
+            html(
+                head_builder(title="Music"),
+                body_builder(
+                    navibar_builder().set_menu({
+                        "Playlist":"",
+                        "":"",
+                        "Create folder":"",
+                        "Upload files":"",
+                        "Delete":""
+                      }),
+                    _section,
+                    _modal,
+                    footer_builder()
+                )
+            )
+        )
